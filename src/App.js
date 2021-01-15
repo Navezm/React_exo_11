f7eb5c511d13dbfb4ea8c8ccfe80531d2f5fc3a3
Navezm <navez.martin@gmail.com>
2021-01-15 10:06:30 +0100

    exo finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react';
+import './app.css';
 
 class App extends Component {
+  increment(){
+    let span = document.querySelector("span");
+    span.innerHTML = Number(span.innerHTML) + 1;
+  }
+
+  decrement(){
+    let span = document.querySelector("span");
+    span.innerHTML = Number(span.innerHTML) - 1;
+  }
+
+  changeClass(){
+    let span = document.querySelector("span");
+    span.classList.toggle("text-red");
+  }
+
   render(){
     return (
       <div className="App">
-       
+        <span>0</span> <br/> <br/>
+        <button onClick={this.increment}>Click +1</button> <button onClick={this.decrement}>Click -1</button> <button onClick={this.changeClass}>Change text red</button>
       </div>
     );
   };

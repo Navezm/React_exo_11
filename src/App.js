fdc23c158c788093cfe32923a69295c96d188f3a
Navezm <navez.martin@gmail.com>
2021-01-19 15:34:32 +0100

    Exo fini

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React, {Component} from 'react';
 import './app.css';
 
 class App extends Component {
-  increment(){
-    let span = document.querySelector("span");
-    span.innerHTML = Number(span.innerHTML) + 1;
-  }
+  state = {
+    count: 0
+  };
 
-  decrement(){
-    let span = document.querySelector("span");
-    span.innerHTML = Number(span.innerHTML) - 1;
-  }
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
 
-  changeClass(){
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  changeClass = () => {
     let span = document.querySelector("span");
     span.classList.toggle("text-red");
-  }
+  };
 
   render(){
     return (
       <div className="App">
-        <span>0</span> <br/> <br/>
+        <span>{this.state.count}</span> <br/> <br/>
         <button onClick={this.increment}>Click +1</button> <button onClick={this.decrement}>Click -1</button> <button onClick={this.changeClass}>Change text red</button>
       </div>
     );
